c1ce7df2e55b230dfa7afe2b1fd6a288cd37a3f7
Snehangshu Karmakar <snehangshu@gmail.com>
2020-08-07 15:30:51 +0530

    Create freq-count.py

---
+++ freq-count.py
@@ -0,0 +1,14 @@
+'''Write a function named frequency_dictionary that takes a list of elements named words as a parameter. The function should return a dictionary containing the frequency of each element in words.'''
+# Write your frequency_dictionary function here:
+def frequency_dictionary(words):
+  freq = {}
+  for word in words:
+    if word not in freq:
+      freq[word] = 0
+    freq[word] += 1
+  return freq  
+# Uncomment these function calls to test your  function:
+print(frequency_dictionary(["apple", "apple", "cat", 1]))
+# should print {"apple":2, "cat":1, 1:1}
+print(frequency_dictionary([0,0,0,0,0]))
+# should print {0:5}
