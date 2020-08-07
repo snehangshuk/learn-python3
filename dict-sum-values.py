7ab6b7c8e95d04e70b2ef91f0e861efd5dc2e254
Snehangshu Karmakar <snehangshu@gmail.com>
2020-08-07 11:02:07 +0530

    Create dict-sum-values.py

---
+++ dict-sum-values.py
@@ -0,0 +1,4 @@
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+total_exercises = 0
+total_exercises = sum(num_exercises.values())
+print(total_exercises) 
