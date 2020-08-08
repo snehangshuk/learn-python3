2245d55c7437e15d478022bf18a5ef59b0dfe251
Snehangshu Karmakar <snehangshu@gmail.com>
2020-08-08 10:55:37 +0530

    Create super-constructor.py

---
+++ super-constructor.py
@@ -0,0 +1,10 @@
+class PotatoSalad:
+  def __init__(self, potatoes, celery, onions):
+    self.potatoes = potatoes
+    self.celery = celery
+    self.onions = onions
+
+class SpecialPotatoSalad(PotatoSalad):
+  def __init__(self, potatoes, celery, onions, raisins = 40):
+    super().__init__(potatoes, celery, onions)
+    self.raisins = raisins
