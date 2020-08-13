627ce7219943ef62778c1ae31bb0c8854cef8d49
Snehangshu Karmakar <snehangshu@gmail.com>
2020-08-13 11:48:31 +0530

    Adds more functionality in Flask app

--- cookbook/app.py
+++ cookbook/app.py
@@ -1,27 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from helper import recipes, descriptions, ingredients, instructions, add_ingredients, add_instructions, comments
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from forms import RecipeForm, CommentForm
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "mysecret"
 
-class CommentForm(FlaskForm):
-  comment =  StringField("Comment")
-  submit = SubmitField("Add Comment")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-  return render_template("index.html", template_recipes=recipes)
+    recipe_form = RecipeForm()
+    if recipe_form.validate_on_submit():
+        new_id = len(recipes)+1
+        recipes[new_id] = recipe_form.recipe.data
+        types[new_id] = recipe_form.recipe_type.data
+        descriptions[new_id] = recipe_form.description.data
+        new_ingredients = recipe_form.ingredients.data
+        new_instructions = recipe_form.instructions.data
+        add_ingredients(new_id, new_ingredients)
+        add_instructions(new_id, new_instructions)
+        comments[new_id] = []
+        #### Redirect to recipe route here
+        return redirect(url_for('recipe', id=new_id))
+    return render_template("index.html", template_recipes=recipes, template_form=recipe_form)
+
 
 @app.route("/recipe/<int:id>", methods=["GET", "POST"])
 def recipe(id):
-  comment_form = CommentForm()
-  #### Process data here
-  new_comment = comment_form.comment.data
-  comments[id].append(new_comment)
-  return render_template("recipe.html", template_recipe=recipes[id], template_description=descriptions[id], template_ingredients=ingredients[id], template_instructions=instructions[id], template_comments=comments[id], template_form=comment_form)
+    comment_form = CommentForm()
+    if comment_form.validate_on_submit():
+        new_comment = comment_form.comment.data
+        comments[id].append(new_comment)
+    return render_template("recipe.html", template_recipe=recipes[id], template_description=descriptions[id],
+                           template_ingredients=ingredients[id], template_instructions=instructions[id],
+                           template_comments=comments[id], template_form=comment_form)
+
 
 @app.route("/about")
 def about():
-  return render_template("about.html")
+    return render_template("about.html")
